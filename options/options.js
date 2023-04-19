e3eb0aac73e83139a3d1abfdbbe083ee00a62b4a
Crowdbotics <engineering@crowdbotics.com>
2023-04-19 05:55:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-limit-dev-77554",
+  url: "https://falling-limit-dev-77554.botics.co",
+  api: "https://falling-limit-dev-77554.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
